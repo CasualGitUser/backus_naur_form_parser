dfd9e409b28e2597ddede3dc9e0be7c24c39de1c
Kartoffel69420 <erik1114g@gmail.com>
2025-01-03 18:37:26 +0100

    updated cargo.toml version

---
+++ target/debug/deps/backus_naur_form_parser_and_compiler-831ca9645e2204d7.d
@@ -0,0 +1,15 @@
+E:\Erik\Hobby Projekte\Rust\backus_naur_tokenizer\target\debug\deps\libbackus_naur_form_parser_and_compiler-831ca9645e2204d7.rmeta: src\lib.rs src\backus_naur_form.rs src\backus_naur_form\rule.rs src\backus_naur_form\symbol.rs src\backus_naur_form\symbol\non_terminal_symbol.rs src\backus_naur_form\token.rs src\backus_naur_form\token\non_terminal_token.rs Cargo.toml
+
+E:\Erik\Hobby Projekte\Rust\backus_naur_tokenizer\target\debug\deps\backus_naur_form_parser_and_compiler-831ca9645e2204d7.d: src\lib.rs src\backus_naur_form.rs src\backus_naur_form\rule.rs src\backus_naur_form\symbol.rs src\backus_naur_form\symbol\non_terminal_symbol.rs src\backus_naur_form\token.rs src\backus_naur_form\token\non_terminal_token.rs Cargo.toml
+
+src\lib.rs:
+src\backus_naur_form.rs:
+src\backus_naur_form\rule.rs:
+src\backus_naur_form\symbol.rs:
+src\backus_naur_form\symbol\non_terminal_symbol.rs:
+src\backus_naur_form\token.rs:
+src\backus_naur_form\token\non_terminal_token.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
---
+++ target/release/deps/backus_naur_form_parser_and_compiler-3e9b13e5a7362c7d.d
@@ -0,0 +1,13 @@
+E:\Erik\Hobby Projekte\Rust\backus_naur_tokenizer\target\release\deps\libbackus_naur_form_parser_and_compiler-3e9b13e5a7362c7d.rmeta: src\lib.rs src\backus_naur_form.rs src\backus_naur_form\rule.rs src\backus_naur_form\symbol.rs src\backus_naur_form\symbol\non_terminal_symbol.rs src\backus_naur_form\token.rs src\backus_naur_form\token\non_terminal_token.rs
+
+E:\Erik\Hobby Projekte\Rust\backus_naur_tokenizer\target\release\deps\libbackus_naur_form_parser_and_compiler-3e9b13e5a7362c7d.rlib: src\lib.rs src\backus_naur_form.rs src\backus_naur_form\rule.rs src\backus_naur_form\symbol.rs src\backus_naur_form\symbol\non_terminal_symbol.rs src\backus_naur_form\token.rs src\backus_naur_form\token\non_terminal_token.rs
+
+E:\Erik\Hobby Projekte\Rust\backus_naur_tokenizer\target\release\deps\backus_naur_form_parser_and_compiler-3e9b13e5a7362c7d.d: src\lib.rs src\backus_naur_form.rs src\backus_naur_form\rule.rs src\backus_naur_form\symbol.rs src\backus_naur_form\symbol\non_terminal_symbol.rs src\backus_naur_form\token.rs src\backus_naur_form\token\non_terminal_token.rs
+
+src\lib.rs:
+src\backus_naur_form.rs:
+src\backus_naur_form\rule.rs:
+src\backus_naur_form\symbol.rs:
+src\backus_naur_form\symbol\non_terminal_symbol.rs:
+src\backus_naur_form\token.rs:
+src\backus_naur_form\token\non_terminal_token.rs:
